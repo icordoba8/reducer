546ec9d69c67d90d00c9a91a79e6a09a5d454ca6
isabelino <icordoba8@misena.edu.co>
2020-05-27 21:40:23 -0500

    serve -s build

--- server.js
+++ server.js
@@ -10,4 +10,5 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+//serve -s build
 app.listen(port);
