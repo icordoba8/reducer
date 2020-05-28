a41e331572257f8f678145403d7dda07f58fa178
isabelino <icordoba8@misena.edu.co>
2020-05-27 19:55:31 -0500

    build

---
+++ build/precache-manifest.fb3a274288222a82001a912e51273c09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43c36f2a8ea8584e729fbd4b91b233d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "501f059cf679b48f3726",
+    "url": "/static/css/main.98bd1f93.chunk.css"
+  },
+  {
+    "revision": "d7f1687a17e633926358",
+    "url": "/static/js/2.9efccb94.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.9efccb94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501f059cf679b48f3726",
+    "url": "/static/js/main.dd115e0a.chunk.js"
+  },
+  {
+    "revision": "686b76064226c85fbcd3",
+    "url": "/static/js/runtime-main.6425939c.js"
+  }
+]);
